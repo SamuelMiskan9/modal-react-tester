5821026919966a86596d4a547ae7456e4a10d077
unknown <2001020037@student.umrah.ac.id>
2022-11-14 17:17:25 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AddPost from './components/AddPost';
+
+function App() {
+  return (
+    <div className="App">
+      <AddPost/>
+
+    </div>
+  );
+}
+
+export default App;
